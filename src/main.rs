9aafdf13921bd18cf718a71c5b0b0bd4fac75a38
Dan Knutson <19314778+Giesch@users.noreply.github.com>
2026-02-10 10:26:26 -0500

    create a vulkan instance

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::ffi::CString;
+
+use ash::vk;
+use sdl3::video::Window;
+
 fn main() {
-    println!("Hello, world!");
+    todo!();
+}
+
+struct Renderer;
+
+impl Renderer {
+    fn init(window: Window) -> Result<Self, Box<dyn std::error::Error>> {
+        let entry = ash::Entry::linked();
+
+        let app_info = vk::ApplicationInfo::default()
+            .application_name(c"Vulkan Tutorial")
+            .api_version(vk::API_VERSION_1_3);
+
+        let mut enabled_extension_names = vec![];
+        let window_required_extensions: Vec<_> = window
+            .vulkan_instance_extensions()?
+            .into_iter()
+            .map(|s| CString::new(s).unwrap())
+            .collect();
+        for name in &window_required_extensions {
+            enabled_extension_names.push(name.as_ptr())
+        }
+
+        let create_info = vk::InstanceCreateInfo::default()
+            .application_info(&app_info)
+            .enabled_extension_names(&enabled_extension_names);
+
+        let instance = unsafe { entry.create_instance(&create_info, None)? };
+
+        Ok(Self)
+    }
 }
